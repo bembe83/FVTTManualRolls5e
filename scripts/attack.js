57fa6e73517c8398a2f0f1ff57f6664383748a31
bembe <bembe83@libero.it>
2021-11-28 17:38:12 +0100

    Improved Attack Macro

--- scripts/attack.js
+++ scripts/attack.js
@@ -1,20 +1,15 @@
 let rollTypeTemplate = `
-	<div style="display: flex; flex-direction:column;">
-	    <div style="flex:1">Choose weapon <select id="attackType"></select><br/><br/></div>
-	    <span style="flex:1">Bonus Action <input type="checkbox" id="isBonusAct"/><br/><br/></span>
-	    <span style="flex:1">Hit Dice Roll <input id="hit_roll" type="text" style="width:50px;float:center" value="1" /><br/><br/></span>
+	<div style="display: flex; flex-direction:column; align-item: baseline;">
+	    <div style="flex:1; margin-bottom: 5px;">Choose weapon <select id="attackType"></select></div>
+	    <div style="flex:1; margin-bottom: 5px;">Bonus Action <input type="checkbox" id="isBonusAct"/></div>
+	    <div style="flex:1; margin-bottom: 5px;">Hit Dice Roll <input id="hit_roll" type="text" style="width:50px; float:center" value="1" placeholder="Number between 1 and 20"/></div>
 	</div>`;
 	
 let hitTemplate = `
-	<div style="display:flex">
-        <span style="flex:1">Damage Roll <input id="dam_roll" type="text" style="width:50px;float:center" /></span>
+	<div style="display:flex; flex-direction:column; align-item: baseline;">
+        <div style="flex:1; margin-bottom: 5px;">Damage Roll <input id="dam_roll" type="text" style="width:50px;float:center" placeholder="Number between 1 and <<dice_face>>"/></div>
+        <div style="flex:1; margin-bottom: 5px;">Damage Roll Versatile<input id="dam_roll" type="text" style="width:50px;float:center; block:none" placeholder="Number between 1 and <<dice_face_vers>>"/></div>
     </div>
-`;
-
-let chatTemplate =`
-	<div>Rolled: <span style="color: ${hit_color}">${result}</span> against ${ac} Target Armor</div>
-	<div>It was a${crit_hit} Hit!</div>
-	<div><button id="rollDamage">Roll Damage</button></div>
 `;
 
 main();
@@ -24,18 +19,18 @@ async function main(){
 	let d;
 	if(canvas.tokens.controlled.length == 0 || canvas.tokens.controlled.length > 1)
 	{
-		ui.notification("Please select a signle token for the attack");
+		ui.notifications.error("Please select a signle token for the attack");
 		return;
 	}
 
 	let targets = Array.from(game.user.targets);
 	if(targets.length == 0)
 	{
-		ui.notification("Please select a target token for the attack");
+		ui.notifications.error("Please select a target token for the attack");
 		return;
 	}
 
-	let actorWeapons = actor.data.items.filter(item => attackType.includes(item.data.data.actionType)); 
+	let actorWeapons = actor.data.items.filter(item => attackType.includes(item.data.data.actionType) && item.data.data.equipped); 
 
 	d = new Dialog({
 		title: `Attack Menu`,
@@ -48,9 +43,13 @@ async function main(){
 		            let weapon_key = html.find('[id=attackType]')[0].value;
 		            let isbonusact = html.find('[id=isBonusAct]')[0].value;
 		            let hit_roll = html.find('[id=hit_roll]')[0].value;
-		            //let dam_roll = html.find('[id=dam_roll]')[0].value;
-		            Attack(weapon_key, isbonusact, hit_roll);
-		            close();
+		            if(Number(hit_roll)<1 || Number(hit_roll)>20 || Number(hit_roll) === NaN)
+		            	ui.notification.error("Enter a valid number between 1 and 20 as roll for a d20 dice");
+		            else
+		            {
+		            	Attack(weapon_key, isbonusact, hit_roll);
+		            	close();
+		            }
 	            }       
 			},
 			close: {
@@ -71,6 +70,8 @@ async function main(){
 
 function Attack(weapon_key, isbonusact, hit_value)
 {
+	let hitDice = "1d20";
+	let diceFaces = hitDice.split("d")[1];
 	let prof=0;
 	let hit_mod = 0;
 	let hit_bonus = 0;
@@ -78,13 +79,20 @@ function Attack(weapon_key, isbonusact, hit_value)
 	let dam_bonus = 0;
 
 	let actor = canvas.tokens.controlled[0].actor;
-	let targets = Array.from(game.user.targets).map(token => token.actor);
-	
-	console.log(targets);
-	
+	let actorImg = canvas.tokens.controlled[0].data.img;
+	let targets = Array.from(game.user.targets).map(token => token.actor);	
 	let weapon = actor.data.items.filter(function(item){return item.data.name == weapon_key;})[0];
 	let actionType = weapon.data.data.actionType;
 	let isProf = weapon.data.data.proficient;
+	let weaponName = weapon.data.name;
+	let weaponDesc = weapon.data.data.description.chat;
+	let weaponDamDice = weapon.data.data.damage.parts[0][0];
+	weaponDamDice = weaponDamDice.replace(weaponDamDice.match("\[[a-zA-Z]*\]"),"").replace(/\s{0,}/g, '').split("+")[0];
+	let weaponDamDiceVers = weapon.data.data.damage.versatile?weapon.data.data.damage.versatile:"0";
+	weaponDamDiceVers = weaponDamDiceVers.replace(weaponDamDiceVers.match("\[[a-zA-Z]*\]"),"").replace(/\s{0,}/g, '').split("+")[0];
+		
+	console.log(weaponDamDice);
+	console.log(weaponDamDiceVers);
 
 	if(isProf)
 		prof = actor.data.data.prof;
@@ -117,76 +125,147 @@ function Attack(weapon_key, isbonusact, hit_value)
 		hit_mod = 0;
 	}
 
-	let hit_color = "#000000";
+	let hit_class = "dice-total";
+	let dice_class = "roll die d"+diceFaces;
 	let crit_hit = "";
-	if (hit_value == "20")
+	if (hit_value == diceFaces)
 	{
-		hit_color = "#00aa05";
+		hit_class = hit_class + " critical";
+		dice_class = dice_class + " max";
 		crit_hit = " Critical";
 	}
 	else if(hit_value == "1")
 	{
-		hit_color = "#e50303";
+		hit_class = hit_class + " fumble";
 		crit_hit = " Critical";
+		dice_class = dice_class + " min";
 	}
 
 	let hit_roll = new Roll(`${hit_value}+${prof}+${hit_mod}+${hit_bonus}`).evaluate({async:false});
 
 	for(let target of targets)
 	{
-		console.log(target);
+		let targetName =  target.data.name;
 		let ac = target.data.data.attributes.ac.value;
 		let result = hit_roll.total;
 		
-		if(result>ac)
-		{
-			chatTemplate = `
-				<p> Rolled: <span style="color: ${hit_color}">${result}</span> against ${ac} Target Armor </p>
-				<p> It was a${crit_hit} Hit! </p>
-				<p> <button id="rollDamage">Roll Damage</button></p>
-				`;
-		} else {
-			chatTemplate = `
-				<p> Rolled: <span style="color: ${hit_color}">${result}</span> against ${ac} Target Armor </p>
-				<p> It was a${crit_hit} Miss! </p>
-				`;
-		}
+		let chatTemplate =`
+        <div style="border: 1px;">
+	        <span> <img src="${actorImg}" style="width: 60px; height:60px; object-fit: cover; "/></span>
+	        <span id="weaponName"><h2>${weaponName}</h2></span>
+	        <span id="weaponDesc" style="display: none;">${weaponDesc}<br/></span>
+        </div>		
+		<div class="dice-roll">
+			<div class="dice-result">
+		        <div class="dice-formula">${hitDice}+${prof}+${hit_mod}+${hit_bonus}</div>
+		        <div class="dice-tooltip" style="display: none;">
+		            <section class="tooltip-part">
+		                <div class="dice">
+		                    <header class="part-header flexrow">
+		                        <span class="part-formula">${hitDice}</span>
+		                        <span class="${hit_class}" style="word-break: normal;">${hit_value}</span>
+		                    </header>
+		                    <ol class="dice-rolls">
+		                    	<li class="${dice_class}">${hit_value}</li>
+		                    </ol>
+		                </div>
+		            </section>
+		        </div>
+		        <h4 class="${hit_class}">${result}</h4>
+		    </div>
+		</div>
+		<div id="hit" style="display:none; margin-top: 15px; margin-bottom: 5px; border: inset outset;"><h3>${crit_hit} Hit the target ${targetName}</h3></div>
+	    <div id="miss" style="display:none; margin-top: 15px; margin-bottom: 5px; border: inset outset;"><h3>${crit_hit} Miss the target ${targetName}</h3></div>
+	    <div><button id="rollDamage" style="display:none">Roll Damage</button></div>
+		`;
+		
 		ChatMessage.create({
 			speaker: {
 				alias: actor.name
 			},
 			content: chatTemplate,
-			roll: hit_roll
+			roll: hit_roll,
 		});
+		
 		Hooks.once('renderChatMessage', (chatItem, html) => {
-			html.find("#rollDamage").click(() => {
-			    new Dialog({
-			        title:"Enter damage roll",
-			        content: hitTemplate,
-			        buttons: {
-				        ok: {
-						    label: `Damage`,
-						    callback: (html) =>
-			                {
-				                let dam_roll = html.find('[id=dam_roll]')[0].value;
-				                let damage_roll = new Roll(`${dam_roll}+${hit_mod}+${hit_bonus}`);
-				                damage_roll.toMessage({
-					                speaker: {
-						                alias: actor.name
-					                }
-					            });
-				                close();
-			                }
-		                },
-				        close: {
-					        icon: "<i class='fas fa-tick'></i>",
-					        label: `Close`
-				        }
-			        },
-				    default: "close",
-					close: () => {}
-			   }).render(true);    
+		    html.find("#weaponName").click(()=>{
+			    if(html.find("#weaponDesc")[0].style.display == "none")
+					html.find("#weaponDesc")[0].style.display="block";
+				else
+					html.find("#weaponDesc")[0].style.display="none";
 			});
+			if(result > ac)
+			{
+				let damDiceFaces = weaponDamDice.split("d")[1]? weaponDamDice.split("d")[1]: 0;
+				let damVersDiceFaces = weaponDamDiceVers.split("d")[1]?weaponDamDiceVers.split("d")[1]:0;
+				html.find("#hit")[0].style.display = "block";
+				html.find("#rollDamage")[0].style.display = "block";
+				html.find("#rollDamage").click(() => {
+				    new Dialog({
+				        title:"Enter damage roll",
+				        content: hitTemplate.replace("<<dice_face>>",damDiceFaces).replace("<<dice_face_vers>>",damVersDiceFaces),
+				        buttons: {
+					        ok: {
+							    label: `Damage`,
+							    callback: (html) =>
+				                {
+					                let dam_roll = html.find('[id=dam_roll]')[0].value;
+					                let damage_roll = new Roll(`${dam_roll}+${hit_mod}+${hit_bonus}`).evaluate({async:false});
+					                let diceDamFaces = weaponDamDice.split("d")[1];
+					                let hit_class = "dice-total";
+					                let dice_class = "roll die d"+diceDamFaces;
+					            	if (dam_roll == diceFaces)
+					            	{
+					            		hit_class = hit_class + " critical";
+					            		dice_class = dice_class + " max";
+					            	}
+					            	else if(dam_roll == "1")
+					            	{
+					            		hit_class = hit_class + " fumble";
+					            		dice_class = dice_class + " min";
+					            	}
+					        		let DamRollTemplate =`
+					        			<div class="dice-roll">
+					        				<div class="dice-result">
+					        					<div class="dice-formula">${weaponDamDice}+${hit_mod}+${hit_bonus}</div>
+					        					<div class="dice-tooltip" style="display: none;">
+					        						<section class="tooltip-part">
+					        							<div class="dice">
+					        								<header class="part-header flexrow">
+					        									<span class="part-formula">${weaponDamDice}</span>
+					        									<span class="${hit_class}" style="word-break: normal;">${dam_roll}</span>
+					        								</header>
+					        								<ol class="dice-rolls">
+					        									<li class="${dice_class}">${dam_roll}</li>	
+					        								</ol>
+					        							</div>
+					        						</section>
+					        					</div>
+					        					<h4 class="${hit_class}">${damage_roll.total}</h4>
+					        				</div>
+					        			</div>
+					        		`;
+					                damage_roll.toMessage({
+					                	content: DamRollTemplate,
+						                speaker: {
+							                alias: actor.name
+						                }
+						            });
+					                close();
+				                }
+			                },
+					        close: {
+						        icon: "<i class='fas fa-tick'></i>",
+						        label: `Close`
+					        }
+				        },
+					    default: "close",
+						close: () => {}
+				   }).render(true);    
+				});
+			}
+			else
+				html.find("#miss")[0].style.display = "block";
 		});
 	}
 	return;
